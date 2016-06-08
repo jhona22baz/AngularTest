e8912db741970c6cd4f178037def2e3c96be5d8c
Jhonatan Bazaldua <jhona.22.baz@gmail.com>
2016-06-08 14:41:36 -0500

    add controllers form the config archive

--- angular1.10/README.md
+++ angular1.10/README.md
@@ -1,2 +1,3 @@
 ngrout - para hacer el routing de las paginas
 ng-view - para poner el contenido de las paginas en mi config.js
+add Controller to mark menu's as active
--- angular1.10/js/app.js
+++ angular1.10/js/app.js
@@ -3,5 +3,12 @@ var app = angular.module('universidadApp',['ngRoute']);
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
 	$scope.menuSuperior = 'parciales/menu.html';
+	$scope.setActive = function(Opcion){
+		$scope.mInicio = "";
+		$scope.mProfesores = "";
+		$scope.mAlumnos = "";
+
+		$scope[Opcion] = "active";
+	}
 
 }]);
